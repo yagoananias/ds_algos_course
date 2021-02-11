18e4b07f1a57cfc697ff345988a6aa73d7413ed9
Yago Ananias <yago_souza92@yahoo.com.br>
2021-02-11 01:15:04 -0300

    Adding the Delete method

--- array_javascript.js
+++ array_javascript.js
@@ -1,3 +1,5 @@
+// implementing an array in JS
+
 class MyArray {
   constructor() {
     this.length = 0;
@@ -18,11 +20,26 @@ class MyArray {
     const lastItem = this.data[this.length-1]
     delete this.data[this.length-1]
     this.length--;
-    return item;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++ ) {
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.length-1];
+    this.length--;
   }
 }
 
 const newArray = new MyArray();
 newArray.push('Yago');
 newArray.push('Andressa');
+newArray.delete(1);
+newArray.push('Teste');
 console.log(newArray);
