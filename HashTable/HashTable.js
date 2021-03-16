5c3aad35ffb2a43dcdf25ed08e51e39ef87acfe3
Yago Ananias <yago.crazy@hotmail.com>
2021-03-16 16:51:20 -0300

    Update HashTable.js

--- HashTable/HashTable.js
+++ HashTable/HashTable.js
@@ -34,6 +34,16 @@ class HashTable {
       }
     }
   }
+  
+  keys() {
+    const keysArray = [];
+    for(let i = 0; i < this.data.length; i++) {
+      if(this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
 }
 
 const myHashTable = new HashTable(50);
