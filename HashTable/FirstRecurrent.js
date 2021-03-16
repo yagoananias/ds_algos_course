8474ee5a4eca86da03b4b02cb6b3890c4b37ae54
Yago Ananias <yago.crazy@hotmail.com>
2021-03-16 18:45:33 -0300

    Update FirstRecurrent.js

--- HashTable/FirstRecurrent.js
+++ HashTable/FirstRecurrent.js
@@ -8,5 +8,16 @@
 // It should return undefined
 
 class FirstRecurrent {
+  a = [2,5,1,2,3,5,1,2,4];
+  b = [2,1,1,2,3,5,1,2,4];
+  c = [2,4,5];
   
+  isRecurrent(a) {
+    let counter = 0;
+    for(let i = 0; i < a.length; i++) {
+      if(a[i] == counter) {
+        counter++;
+      }
+    }
+  }
 }
