e5cd59cdb2fde7e34a978fd3b0da5f2786d486ed
Yago Ananias <yago.crazy@hotmail.com>
2021-03-16 16:56:46 -0300

    Update FirstRecurrent.js

--- HashTable/FirstRecurrent.js
+++ HashTable/FirstRecurrent.js
@@ -1,1 +1,12 @@
+//Given an array = [2,5,1,2,3,5,1,2,4]
+// It should return 2
 
+//Given an array = [2,1,1,2,3,5,1,2,4]
+// It should return 1
+
+//Given an array = [2,4,5]
+// It should return undefined
+
+class FirstRecurrent {
+  
+}
