a3fdf218f7f1e050fbe9ee039f0db74926e030bc
Yago Ananias <yago.crazy@hotmail.com>
2021-03-16 05:38:35 -0300

    Update HashTable.js

--- HashTable/HashTable.js
+++ HashTable/HashTable.js
@@ -6,8 +6,7 @@ class HashTable {
   _hash(key) {
     let hash = 0;
     for(let i = 0, i < key.length; i++ ) {
-      hash = (hash + key.charCodeAt(i) * i) %
-      this.data.length
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
     }
     return hash;
   }
