2801f110ab29f0d41b0a35444d3ef4ddb684bbaf
Yago Ananias <yago.crazy@hotmail.com>
2021-03-17 19:08:51 -0300

    Update MyArray.js

--- Arrays/MyArray.js
+++ Arrays/MyArray.js
@@ -1,1 +1,26 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  
+  get(index) {
+    return this.data[index];
+  }
+  
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return item;
+  }
+}
+
+const newArray = new MyArray();
 
