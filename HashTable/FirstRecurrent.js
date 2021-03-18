490e49aa8949e5d729e96d64b55b9ef1960e1ebe
Yago Ananias <yago.crazy@hotmail.com>
2021-03-18 05:03:02 -0300

    Update FirstRecurrent.js

--- HashTable/FirstRecurrent.js
+++ HashTable/FirstRecurrent.js
@@ -7,17 +7,15 @@
 //Given an array = [2,4,5]
 // It should return undefined
 
-// a = [2,5,1,2,3,5,1,2,4];
-// b = [2,1,1,2,3,5,1,2,4];
-//c = [2,4,5];
-
-class FirstRecurrent {
-  
-  isRecurrent(a) {
-    
+function firstRecurringCharacter(input) {
+  for(let i = 0; i < input.length; i++) {
+    for(let j = i+1; j < input.length; j++) {
+      if(input[i] == input[j]) {
+        return input[i];
+      }
+    }
   }
-  
+  return undefined;
 }
 
-const myArray = new FirstRecurrent();
-myArray.isRecurrent(a[2,5,1,2,3,5,1,2,4])
+firstRecurringCharacter([2,5,1,2,3,5,1,2,4])
