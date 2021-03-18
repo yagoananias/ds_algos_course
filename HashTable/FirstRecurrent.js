763389e57c0257cc847074634793d43abf1cf9a0
Yago Ananias <yago.crazy@hotmail.com>
2021-03-18 05:05:15 -0300

    Update FirstRecurrent.js

--- HashTable/FirstRecurrent.js
+++ HashTable/FirstRecurrent.js
@@ -7,6 +7,8 @@
 //Given an array = [2,4,5]
 // It should return undefined
 
+// This is a NAIVE solution
+
 function firstRecurringCharacter(input) {
   for(let i = 0; i < input.length; i++) {
     for(let j = i+1; j < input.length; j++) {
