5d54081c5d7f2da9bc673f41c88dffb56eea92e6
Yago Ananias <yago.crazy@hotmail.com>
2021-08-03 23:52:29 -0300

    Update SearchingAlgorithms.js

--- SearchingAlgorithms.js
+++ SearchingAlgorithms.js
@@ -1,1 +1,14 @@
+//linear search
+ let linearSearch = (list,value)=>{
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
 
+var list =  [12, 45, 48, 5, 451, 2,34 ,43,54,66 ]
+var value = 2;
+
+linearSearch(list , value) // result should 5
